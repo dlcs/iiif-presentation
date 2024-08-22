b4c5c4894be8d94b0ce91fb850f4416fe33df43c
jack.lewis <jack.lewis@digirati.com>
2024-08-22 09:27:26 +0100

    initial commit containing new collection

---
+++ src/IIIFPresentation/API/Features/Storage/Requests/UpdateCollection.cs
@@ -0,0 +1,25 @@
+﻿using API.Converters;
+using API.Infrastructure.Requests;
+using MediatR;
+using Models.API.Collection;
+
+namespace API.Features.Storage.Requests;
+
+public class UpdateCollection : IRequest<ModifyEntityResult<FlatCollection>>
+{
+    public UpdateCollection(int customerId, string collectionId, FlatCollection collection, UrlRoots urlRoots)
+    {
+        CustomerId = customerId;
+        CollectionId = collectionId;
+        Collection = collection;
+        UrlRoots = urlRoots;
+    }
+
+    public int CustomerId { get; }
+    
+    public string CollectionId { get; set; }
+    
+    public FlatCollection Collection { get; }
+    
+    public UrlRoots UrlRoots { get; }
+}
