b8e8c9cfab28bf52e13d73256daab48b2a67d3f3
jack.lewis <jack.lewis@digirati.com>
2024-10-09 09:47:08 +0100

    push up wip for hierarchy table

---
+++ src/IIIFPresentation/Models/Database/General/Hierarchy.cs
@@ -0,0 +1,26 @@
+﻿
+using Models.Database.Collections;
+
+namespace Models.Database.General;
+
+public class Hierarchy
+{
+    public int Id { get; set; }
+
+    public string? CollectionId { get; set; }
+    
+    public Collection? Collection { get; set; }
+    
+    public string? ManifestId { get; set; }
+    
+    public Manifest? Manifest { get; set; }
+    public required string Slug { get; set; }
+    
+    public string? Parent { get; set; }
+    
+    public int? ItemsOrder { get; set; }
+    
+    public bool Public { get; set; }
+    
+    public bool Canonical { get; set; }
+}
