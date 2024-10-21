6cb022e9e09d6346f541cc7e02e0bb2c3ecfade6
p-kaczynski <p.a.kaczynski@gmail.com>
2024-10-21 13:46:44 +0100

    Update IProcessJson.cs

--- src/IIIFPresentation/API/Converters/Streaming/IProcessJson.cs
+++ src/IIIFPresentation/API/Converters/Streaming/IProcessJson.cs
@@ -4,6 +4,20 @@ namespace API.Converters.Streaming;
 
 public interface IProcessJson
 {
+    /// <summary>
+    ///     Called once when starting to process a JSON stream
+    /// </summary>
+    /// <returns>Arbitrary object that will be ref passed to <see cref="OnToken" /></returns>
     object GetInitialState();
+
+    /// <summary>
+    ///     Called when a full JSON token has been read from input Stream
+    /// </summary>
+    /// <param name="reader">Current reader that read the JSON token</param>
+    /// <param name="writer">Matching writer for output Stream</param>
+    /// <param name="customState">
+    ///     State, as initially obtained from <see cref="GetInitialState" />, or one created in previous
+    ///     <see cref="OnToken" /> incovations
+    /// </param>
     void OnToken(ref Utf8JsonReader reader, Utf8JsonWriter writer, ref object customState);
 }
