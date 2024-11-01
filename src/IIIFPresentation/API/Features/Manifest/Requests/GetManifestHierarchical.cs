09e011b9e6d0ae3ad7a60e352fd36951439f3d35
p-kaczynski <p.a.kaczynski@gmail.com>
2024-11-01 14:51:34 +0000

    Fix Json processor to work with S3 streams and apply to manifest processing for speed improvement

--- src/IIIFPresentation/API/Features/Manifest/Requests/GetManifestHierarchical.cs
+++ src/IIIFPresentation/API/Features/Manifest/Requests/GetManifestHierarchical.cs
@@ -1,17 +1,19 @@
 ﻿using API.Converters;
+using API.Converters.Streaming;
 using API.Helpers;
-using API.Infrastructure.AWS;
+using AWS.S3;
+using AWS.Settings;
+using Core.Streams;
 using MediatR;
-using Models.API.Manifest;
+using Microsoft.Extensions.Options;
 using Models.Database.General;
-using Repository;
 
 namespace API.Features.Manifest.Requests;
 
 public class GetManifestHierarchical(
     Hierarchy hierarchy,
     string slug,
-    UrlRoots urlRoots) : IRequest<IIIF.Presentation.V3.Manifest?>
+    UrlRoots urlRoots) : IRequest<string?>
 {
     public Hierarchy Hierarchy { get; } = hierarchy;
     public string Slug { get; } = slug;
@@ -19,31 +21,36 @@ public class GetManifestHierarchical(
 }
 
 public class GetManifestHierarchicalHandler(
-    IMediator mediator,
-    PresentationContext dbContext,
-    IIIFS3Service iiifS3) : IRequestHandler<GetManifestHierarchical, IIIF.Presentation.V3.Manifest?>
+    IBucketReader bucketReader,
+    IOptions<AWSSettings> options) : IRequestHandler<GetManifestHierarchical, string?>
 {
+    private readonly AWSSettings settings = options.Value;
+
     #region Implementation of IRequestHandler<in GetManifestHierarchical,PresentationManifest?>
 
-    public async Task<IIIF.Presentation.V3.Manifest?> Handle(GetManifestHierarchical request,
+    public async Task<string?> Handle(GetManifestHierarchical request,
         CancellationToken cancellationToken)
-    {
+    {          
+
         var flatId = request.Hierarchy.ManifestId ??
                      throw new InvalidOperationException(
                          "The differentiation of requests should prevent this from happening.");
 
-        var manifest = await iiifS3.ReadIIIFFromS3<IIIF.Presentation.V3.Manifest>(
-            BucketHelperX.GetManifestBucketKey(request.Hierarchy.CustomerId, flatId), cancellationToken);
 
-        if (manifest == null)
+        var objectFromS3 = await bucketReader.GetObjectFromBucket(
+            new(settings.S3.StorageBucket, BucketHelperX.GetManifestBucketKey(request.Hierarchy.CustomerId, flatId)),
+            cancellationToken);
+
+        if (objectFromS3.Stream.IsNull())
             return null;
 
-        manifest.Id = new UriBuilder(request.UrlRoots.BaseUrl!)
-        {
-            Path = $"{request.Hierarchy.CustomerId}/{request.Slug}"
-        }.Uri.ToString();
-
-        return manifest;
+        using var memoryStream = new MemoryStream();
+        using var reader = new StreamReader(memoryStream);
+        StreamingJsonProcessor.ProcessJson(objectFromS3.Stream, memoryStream,
+            objectFromS3.Headers.ContentLength,
+            new S3StoredJsonProcessor(request.Hierarchy.GenerateHierarchicalId(request.UrlRoots)));
+        memoryStream.Seek(0, SeekOrigin.Begin);
+        return await reader.ReadToEndAsync(cancellationToken);
     }
 
     #endregion
