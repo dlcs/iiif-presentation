12d01e1525032675b10822376e3dd95ef9fd0534
Donald Gray <donald.gray@digirati.com>
2024-11-28 16:43:51 +0000

    Use IdentityManager in favour of extension method
    Extension was marked [Obsolete] in previous commit, removing

--- src/IIIFPresentation/API/Helpers/CollectionHelperX.cs
+++ src/IIIFPresentation/API/Helpers/CollectionHelperX.cs
@@ -1,10 +1,4 @@
-﻿using System.Data;
-using API.Converters;
-using API.Infrastructure.IdGenerator;
-using Microsoft.EntityFrameworkCore;
-using Models.Database;
-using Models.Database.Collections;
-using Models.Database.General;
+﻿using Models.Database.Collections;
 
 namespace API.Helpers;
 
@@ -13,7 +7,6 @@ namespace API.Helpers;
 /// </summary>
 public static class CollectionHelperX
 {
-    private const int MaxAttempts = 3;
     private const string ManifestsSlug = "manifests";
     private const string CollectionsSlug = "collections";
 
@@ -25,36 +18,4 @@ public static class CollectionHelperX
 
     private static string GetSlug<T>(this T resource) where T : IHierarchyResource
         => resource is Manifest ? ManifestsSlug : CollectionsSlug;
-
-    [Obsolete("Use IdentityManager.GenerateUniqueId instead.")]
-    public static async Task<string> GenerateUniqueIdAsync<T>(this DbSet<T> entities,
-        int customerId, IIdGenerator idGenerator, CancellationToken cancellationToken = default)
-        where T : class, IIdentifiable
-    {
-        var isUnique = false;
-        var id = string.Empty;
-        var currentAttempt = 0;
-        var random = new Random();
-        var maxRandomValue = 25000;
-
-        while (!isUnique)
-        {
-            if (currentAttempt > MaxAttempts)
-            {
-                throw new ConstraintException("Max attempts to generate an identifier exceeded");
-            }
-
-            id = idGenerator.Generate([
-                customerId,
-                DateTime.UtcNow.Ticks,
-                random.Next(0, maxRandomValue)
-            ]);
-
-            isUnique = !await entities.AnyAsync(e => e.Id == id && e.CustomerId == customerId, cancellationToken);
-
-            currentAttempt++;
-        }
-
-        return id;
-    }
 }
