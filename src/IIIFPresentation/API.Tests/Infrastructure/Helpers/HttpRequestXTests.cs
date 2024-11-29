bc54e67da1182cab7992fb240a344f5676afdef6
Donald Gray <donald.gray@digirati.com>
2024-11-29 13:54:56 +0000

    Helper to check for 'create space' header

--- src/IIIFPresentation/API.Tests/Infrastructure/Helpers/HttpRequestXTests.cs
+++ src/IIIFPresentation/API.Tests/Infrastructure/Helpers/HttpRequestXTests.cs
@@ -6,7 +6,7 @@ namespace API.Tests.Infrastructure.Helpers;
 public class HttpRequestXTests
 {
     [Fact]
-    public void HttpRequestX_False_IfNotFound()
+    public void HasShowExtraHeader_False_IfNotFound()
     {
         var httpRequest = new DefaultHttpContext().Request;
 
@@ -14,7 +14,7 @@ public class HttpRequestXTests
     }
     
     [Fact]
-    public void HttpRequestX_False_IfHeaderPresent_ButUnknownValue()
+    public void HasShowExtraHeader_False_IfHeaderPresent_ButUnknownValue()
     {
         var httpRequest = new DefaultHttpContext().Request;
         httpRequest.Headers.Append("X-IIIF-CS-Show-Extras", "Foo");
@@ -25,11 +25,46 @@ public class HttpRequestXTests
     [Theory]
     [InlineData("X-IIIF-CS-Show-Extras")]
     [InlineData("x-iiif-cs-show-extras")]
-    public void HttpRequestX_True_IfHeaderPresent_AndAllValue(string header)
+    public void HasShowExtraHeader_True_IfHeaderPresent_AndAllValue(string header)
     {
         var httpRequest = new DefaultHttpContext().Request;
         httpRequest.Headers.Append(header, "All");
 
         httpRequest.HasShowExtraHeader().Should().BeTrue();
+    }
+
+    [Fact]
+    public void HasCreateSpaceHeader_False_IfNoLocationHeader()
+    {
+        var httpRequest = new DefaultHttpContext().Request;
+
+        httpRequest.HasCreateSpaceHeader().Should().BeFalse();
+    }
+    
+    [Fact]
+    public void HasCreateSpaceHeader_False_IfHeaderPresent_ButUnknownValue()
+    {
+        var httpRequest = new DefaultHttpContext().Request;
+        httpRequest.Headers.Location = "Foo";
+
+        httpRequest.HasCreateSpaceHeader().Should().BeFalse();
+    }
+    
+    [Fact]
+    public void HasCreateSpaceHeader_True_IfHeaderPresent_AndValueIsCorrectCase()
+    {
+        var httpRequest = new DefaultHttpContext().Request;
+        httpRequest.Headers.Location = "<https://dlcs.io/vocab#Space>;rel=\"DCTERMS.requires\"";
+
+        httpRequest.HasCreateSpaceHeader().Should().BeTrue();
+    }
+    
+    [Fact]
+    public void HasCreateSpaceHeader_False_IfHeaderPresent_AndValueCorrectButWrongCase()
+    {
+        var httpRequest = new DefaultHttpContext().Request;
+        httpRequest.Headers.Location = "<https://dlcs.io/vocab#Space>;rel=\"DCTERMS.requires\"".ToLower();
+
+        httpRequest.HasCreateSpaceHeader().Should().BeFalse();
     }
 }
--- src/IIIFPresentation/API/Infrastructure/Helpers/HttpRequestX.cs
+++ src/IIIFPresentation/API/Infrastructure/Helpers/HttpRequestX.cs
@@ -5,10 +5,20 @@ namespace API.Infrastructure.Helpers;
 public static class HttpRequestX
 {
     private static readonly KeyValuePair<string, string> AdditionalPropertiesHeader = new (CustomHttpHeaders.ShowExtras, "All");
+    private const string CreateSpaceHeader = "<https://dlcs.io/vocab#Space>;rel=\"DCTERMS.requires\"";
 
+    /// <summary>
+    /// Checks if the <see cref="HttpRequest"/> has appropriate header to show additional parameters 
+    /// </summary>
     public static bool HasShowExtraHeader(this HttpRequest request)
     {
         return request.Headers.FirstOrDefault(h => string.Equals(h.Key, AdditionalPropertiesHeader.Key, StringComparison.OrdinalIgnoreCase)).Value ==
                AdditionalPropertiesHeader.Value;
     }
+
+    /// <summary>
+    /// Checks if the <see cref="HttpRequest"/> has header requesting a space be created 
+    /// </summary>
+    public static bool HasCreateSpaceHeader(this HttpRequest request)
+        => request.Headers.Location.Contains(CreateSpaceHeader);
 }
