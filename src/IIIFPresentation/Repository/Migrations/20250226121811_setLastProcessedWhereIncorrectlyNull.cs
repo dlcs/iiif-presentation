1f40642d491b71a973fc22d7edca6d563ca37370
jack.lewis <jack.lewis@digirati.com>
2025-02-26 13:57:45 +0000

    Add migration to set last_processed where it shouldn't be null

---
+++ src/IIIFPresentation/Repository/Migrations/20250226121811_setLastProcessedWhereIncorrectlyNull.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class setLastProcessedWhereIncorrectlyNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+UPDATE manifests
+SET last_processed = now()
+WHERE id NOT IN
+      (SELECT DISTINCT manifest_id
+       FROM batches
+       WHERE finished IS NULL)
+AND last_processed IS NULL;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
