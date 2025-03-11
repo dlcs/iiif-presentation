58c59e36141545d320a8384318f1811056c20bb8
Donald Gray <donald.gray@digirati.com>
2025-03-11 07:05:18 +0000

    Remove commented out code

--- src/IIIFPresentation/API/Features/Storage/Validators/RootCollectionValidator.cs
+++ src/IIIFPresentation/API/Features/Storage/Validators/RootCollectionValidator.cs
@@ -16,11 +16,6 @@ public class RootCollectionValidator : AbstractValidator<PresentationCollection>
             .Empty()
             .WithMessage("Cannot set 'slug' for root collection");
 
-        /* todo - do we need this? Or allow them to specify but it must match the root?
-         RuleFor(pc => pc.PublicId)
-            .Empty()
-            .WithMessage("Cannot specify 'publicId' for root collection");*/
-
         RuleFor(pc => pc.Behavior)
             .Must(behaviors => behaviors?.Contains(Behavior.IsStorageCollection) ?? false)
             .WithMessage($"Root must have '{Behavior.IsStorageCollection}' behavior");
