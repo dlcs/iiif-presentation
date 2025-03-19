c04e44bee1536c8b78fcd033512d7b5e86a76486
Donald Gray <donald.gray@digirati.com>
2025-03-19 14:18:03 +0000

    Use 'IIIF Home' for root label

---
+++ src/IIIFPresentation/Repository/Migrations/20250319141132_rename root label.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class renamerootlabel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Update any 'root' collections that have the default (repository root) label
+            migrationBuilder.Sql(@"
+UPDATE collections
+SET label = '{""en"": [""IIIF Home""]}'
+WHERE id = 'root'
+  AND label = '{""en"": [""(repository root)""]}';");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
