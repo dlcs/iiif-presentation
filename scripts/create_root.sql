0611de624dc0f7327814b118ba7b8b990e20aa7b
Donald Gray <donald.gray@digirati.com>
2025-04-15 11:14:39 +0100

    Update label name in sample script

--- scripts/create_root.sql
+++ scripts/create_root.sql
@@ -1,7 +1,7 @@
 INSERT INTO public.collections 
 	(id, use_path, label, thumbnail, locked_by, created, modified, created_by, modified_by, tags, is_storage_collection, is_public, customer_id) 
 VALUES 
-	('root', true, '{"en": ["(repository root)"]}', 'some/thumb', null, now(),  now(), 'Admin', 'Admin', null, true, true, 1);
+	('root', true, '{"en": ["IIIF Home"]}', null, null, now(),  now(), 'Admin', 'Admin', null, true, true, 1);
 
 INSERT INTO public.hierarchy 
 	(collection_id, manifest_id, type, slug, parent, items_order, canonical, customer_id) 
