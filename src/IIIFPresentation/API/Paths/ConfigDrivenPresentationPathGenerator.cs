2da078c3fecbbb775438ce388848f42b484170da
jack.lewis <jack.lewis@digirati.com>
2025-05-09 12:42:11 +0100

    modifying logging

--- src/IIIFPresentation/API/Paths/ConfigDrivenPresentationPathGenerator.cs
+++ src/IIIFPresentation/API/Paths/ConfigDrivenPresentationPathGenerator.cs
@@ -1,4 +1,5 @@
-﻿using API.Helpers;
+﻿using System.Text.Json;
+using API.Helpers;
 using API.Infrastructure.Requests;
 using Core.Web;
 using Microsoft.Extensions.Options;
@@ -28,8 +29,10 @@ public class ConfigDrivenPresentationPathGenerator(
         string? resourceId = null)
     {
         logger.LogDebug("Get path for presentation");
+        logger.LogDebug("Overrides - {Overrides}", JsonSerializer.Serialize(settings.Overrides));
+        
         var request = GetHttpRequest();
-        logger.LogDebug("request - {Request}", request.ToString());
+        logger.LogDebug("request host - {Request}", request.Host);
         var host = request.Host.Value;
         logger.LogDebug("host - {Host}", host);
         var template = settings.GetPathTemplateForHostAndType(host, presentationServiceType);
