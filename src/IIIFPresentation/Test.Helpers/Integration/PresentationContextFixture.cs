c37b4434a4291bb127cd77db8d30a912b259ac94
jack.lewis <jack.lewis@digirati.com>
2025-05-19 10:13:32 +0100

    move back tests

--- src/IIIFPresentation/Test.Helpers/Integration/PresentationContextFixture.cs
+++ src/IIIFPresentation/Test.Helpers/Integration/PresentationContextFixture.cs
@@ -22,11 +22,6 @@ public class PresentationContextFixture : IAsyncLifetime
     /// </summary>
     public const int CustomerId = 1;
     
-    /// <summary>
-    /// Identity of second default seeded customer
-    /// </summary>
-    public const int SecondCustomerId = 10;
-    
     public PresentationContextFixture()
     {
         var postgresBuilder = new PostgreSqlBuilder()
@@ -42,7 +37,7 @@ public class PresentationContextFixture : IAsyncLifetime
 
     private async Task SeedCustomer()
     {
-        /* This will create for customer 1 and 10
+        /* This will create
          * - root/
          *   - FirstChildCollection/
          *     - SecondChildCollection/
@@ -52,225 +47,248 @@ public class PresentationContextFixture : IAsyncLifetime
          *   - FirstChildManifest/
          *   - FirstChildManifestProcessing/
          */
-
-        var customers = new[] { CustomerId, SecondCustomerId };
-
-        foreach (var customer in customers)
+        
+        // Root collection
+        await DbContext.Collections.AddAsync(new Collection()
         {
-            // Root collection
-            await DbContext.Collections.AddAsync(new Collection
+            Id = RootCollection.Id,
+            UsePath = true,
+            Label = new LanguageMap
             {
-                Id = RootCollection.Id,
-                UsePath = true,
-                Label = new LanguageMap
+                { "en", ["repository root"] }
+            },
+            Thumbnail = "some/location",
+            Created = DateTime.UtcNow,
+            Modified = DateTime.UtcNow,
+            CreatedBy = "admin",
+            Tags = "some, tags",
+            IsStorageCollection = true,
+            IsPublic = true,
+            CustomerId = 10,
+            Hierarchy =
+            [
+                new Hierarchy
                 {
-                    { "en", ["repository root"] }
-                },
-                Thumbnail = "some/location",
-                Created = DateTime.UtcNow,
-                Modified = DateTime.UtcNow,
-                CreatedBy = "admin",
-                Tags = "some, tags",
-                IsStorageCollection = true,
-                IsPublic = true,
-                CustomerId = customer,
-                Hierarchy =
-                [
-                    new Hierarchy
-                    {
-                        Slug = "",
-                        Type = ResourceType.StorageCollection,
-                        Canonical = true
-                    }
-                ]
-            });
-
-            // Child Storage collection
-            await DbContext.Collections.AddAsync(new Collection
+                    Slug = "",
+                    Type = ResourceType.StorageCollection,
+                    Canonical = true
+                }
+            ]
+        });
+        
+        // Root collection
+        await DbContext.Collections.AddAsync(new Collection()
+        {
+            Id = RootCollection.Id,
+            UsePath = true,
+            Label = new LanguageMap
             {
-                Id = "FirstChildCollection",
-                UsePath = true,
-                Label = new LanguageMap
+                { "en", ["repository root"] }
+            },
+            Thumbnail = "some/location",
+            Created = DateTime.UtcNow,
+            Modified = DateTime.UtcNow,
+            CreatedBy = "admin",
+            Tags = "some, tags",
+            IsStorageCollection = true,
+            IsPublic = true,
+            CustomerId = CustomerId,
+            Hierarchy =
+            [
+                new Hierarchy
                 {
-                    { "en", ["first child"] }
-                },
-                Thumbnail = "some/location",
-                Created = DateTime.UtcNow,
-                Modified = DateTime.UtcNow,
-                CreatedBy = "admin",
-                Tags = "some, tags",
-                IsStorageCollection = true,
-                IsPublic = true,
-                CustomerId = customer,
-                Hierarchy =
-                [
-                    new Hierarchy
-                    {
-                        Slug = "first-child",
-                        Parent = RootCollection.Id,
-                        Type = ResourceType.StorageCollection,
-                        Canonical = true
-                    }
-                ]
-            });
+                    Slug = "",
+                    Type = ResourceType.StorageCollection,
+                    Canonical = true
+                }
+            ]
+        });
 
-            // Grandchild storage collection
-            await DbContext.Collections.AddAsync(new Collection()
+        // Child Storage collection
+        await DbContext.Collections.AddAsync(new Collection
+        {
+            Id = "FirstChildCollection",
+            UsePath = true,
+            Label = new LanguageMap
             {
-                Id = "SecondChildCollection",
-                UsePath = true,
-                Label = new LanguageMap
+                {"en", ["first child"] }
+            },
+            Thumbnail = "some/location",
+            Created = DateTime.UtcNow,
+            Modified = DateTime.UtcNow,
+            CreatedBy = "admin",
+            Tags = "some, tags",
+            IsStorageCollection = true,
+            IsPublic = true,
+            CustomerId = CustomerId,
+            Hierarchy =
+            [
+                new Hierarchy
                 {
-                    { "en", ["first child"] }
-                },
-                Thumbnail = "some/location",
-                Created = DateTime.UtcNow,
-                Modified = DateTime.UtcNow,
-                CreatedBy = "admin",
-                Tags = "some, tags",
-                IsStorageCollection = true,
-                IsPublic = true,
-                CustomerId = customer,
-                Hierarchy =
-                [
-                    new Hierarchy
-                    {
-                        Slug = "second-child",
-                        Parent = "FirstChildCollection",
-                        Type = ResourceType.StorageCollection,
-                        Canonical = true
-                    }
-                ]
-            });
+                    Slug = "first-child",
+                    Parent = RootCollection.Id,
+                    Type = ResourceType.StorageCollection,
+                    Canonical = true
+                }
+            ]
+        });
 
-            // Non-public child storage collection
-            await DbContext.Collections.AddAsync(new Collection()
+        // Grandchild storage collection
+        await DbContext.Collections.AddAsync(new Collection()
+        {
+            Id = "SecondChildCollection",
+            UsePath = true,
+            Label = new LanguageMap
             {
-                Id = "NonPublic",
-                UsePath = true,
-                Label = new LanguageMap
+                { "en", ["first child"] }
+            },
+            Thumbnail = "some/location",
+            Created = DateTime.UtcNow,
+            Modified = DateTime.UtcNow,
+            CreatedBy = "admin",
+            Tags = "some, tags",
+            IsStorageCollection = true,
+            IsPublic = true,
+            CustomerId = CustomerId,
+            Hierarchy =
+            [
+                new Hierarchy
                 {
-                    { "en", ["first child - private"] }
-                },
-                Thumbnail = "some/location",
-                Created = DateTime.UtcNow,
-                Modified = DateTime.UtcNow,
-                CreatedBy = "admin",
-                Tags = "some, tags",
-                IsStorageCollection = true,
-                IsPublic = false,
-                CustomerId = customer,
-                Hierarchy =
-                [
-                    new Hierarchy
-                    {
-                        Slug = "non-public",
-                        Parent = RootCollection.Id,
-                        Type = ResourceType.StorageCollection,
-                        Canonical = true
-                    }
-                ]
-            });
+                    Slug = "second-child",
+                    Parent = "FirstChildCollection",
+                    Type = ResourceType.StorageCollection,
+                    Canonical = true
+                }
+            ]
+        });
 
-            // Child IIIF Collection
-            await DbContext.Collections.AddAsync(new Collection()
+        // Non-public child storage collection
+        await DbContext.Collections.AddAsync(new Collection()
+        {
+            Id = "NonPublic",
+            UsePath = true,
+            Label = new LanguageMap
             {
-                Id = "IiifCollection",
-                UsePath = true,
-                Label = new LanguageMap
+                {"en", ["first child - private"] }
+            },
+            Thumbnail = "some/location",
+            Created = DateTime.UtcNow,
+            Modified = DateTime.UtcNow,
+            CreatedBy = "admin",
+            Tags = "some, tags",
+            IsStorageCollection = true,
+            IsPublic = false,
+            CustomerId = CustomerId,
+            Hierarchy =
+            [
+                new Hierarchy
                 {
-                    { "en", ["first child - iiif"] }
-                },
-                Thumbnail = "some/location",
-                Created = DateTime.UtcNow,
-                Modified = DateTime.UtcNow,
-                CreatedBy = "admin",
-                Tags = "some, tags",
-                IsStorageCollection = false,
-                IsPublic = true,
-                CustomerId = customer,
-                Hierarchy =
-                [
-                    new Hierarchy
-                    {
-                        Slug = "iiif-collection",
-                        Parent = RootCollection.Id,
-                        Type = ResourceType.IIIFCollection,
-                        Canonical = true
-                    }
-                ]
-            });
+                    Slug = "non-public",
+                    Parent = RootCollection.Id,
+                    Type = ResourceType.StorageCollection,
+                    Canonical = true
+                }
+            ]
+        });
 
-            // Child IIIF Collection with items
-            await DbContext.Collections.AddAsync(new Collection()
+        // Child IIIF Collection
+        await DbContext.Collections.AddAsync(new Collection()
+        {
+            Id = "IiifCollection",
+            UsePath = true,
+            Label = new LanguageMap
             {
-                Id = "IiifCollectionWithItems",
-                UsePath = true,
-                Label = new LanguageMap
+                {"en", ["first child - iiif"] }
+            },
+            Thumbnail = "some/location",
+            Created = DateTime.UtcNow,
+            Modified = DateTime.UtcNow,
+            CreatedBy = "admin",
+            Tags = "some, tags",
+            IsStorageCollection = false,
+            IsPublic = true,
+            CustomerId = CustomerId,
+            Hierarchy =
+            [
+                new Hierarchy
                 {
-                    { "en", ["first child - iiif"] }
-                },
-                Thumbnail = "some/location",
-                Created = DateTime.UtcNow,
-                Modified = DateTime.UtcNow,
-                CreatedBy = "admin",
-                Tags = "some, tags",
-                IsStorageCollection = false,
-                IsPublic = true,
-                CustomerId = customer,
-                Hierarchy =
-                [
-                    new Hierarchy
-                    {
-                        Slug = "iiif-collection-with-items",
-                        Parent = RootCollection.Id,
-                        Type = ResourceType.IIIFCollection,
-                        Canonical = true
-                    }
-                ]
-            });
-
-            // Child manifest
-            await DbContext.Manifests.AddAsync(new Manifest
+                    Slug = "iiif-collection",
+                    Parent = RootCollection.Id,
+                    Type = ResourceType.IIIFCollection,
+                    Canonical = true
+                }
+            ]
+        });
+        
+        // Child IIIF Collection with items
+        await DbContext.Collections.AddAsync(new Collection()
+        {
+            Id = "IiifCollectionWithItems",
+            UsePath = true,
+            Label = new LanguageMap
             {
-                Id = "FirstChildManifest",
-                CustomerId = customer,
-                Created = DateTime.UtcNow,
-                Modified = DateTime.UtcNow,
-                CreatedBy = "admin",
-                Hierarchy =
-                [
-                    new Hierarchy
-                    {
-                        Slug = "iiif-manifest",
-                        Parent = RootCollection.Id,
-                        Type = ResourceType.IIIFManifest,
-                        Canonical = true
-                    }
-                ],
-                LastProcessed = DateTime.UtcNow
-            });
+                {"en", ["first child - iiif"] }
+            },
+            Thumbnail = "some/location",
+            Created = DateTime.UtcNow,
+            Modified = DateTime.UtcNow,
+            CreatedBy = "admin",
+            Tags = "some, tags",
+            IsStorageCollection = false,
+            IsPublic = true,
+            CustomerId = CustomerId,
+            Hierarchy =
+            [
+                new Hierarchy
+                {
+                    Slug = "iiif-collection-with-items",
+                    Parent = RootCollection.Id,
+                    Type = ResourceType.IIIFCollection,
+                    Canonical = true
+                }
+            ]
+        });
 
-            // processing child manifest
-            await DbContext.Manifests.AddAsync(new Manifest
-            {
-                Id = "FirstChildManifestProcessing",
-                CustomerId = customer,
-                Created = DateTime.UtcNow,
-                Modified = DateTime.UtcNow,
-                CreatedBy = "admin",
-                Hierarchy =
-                [
-                    new Hierarchy
-                    {
-                        Slug = "iiif-manifest-processing",
-                        Parent = RootCollection.Id,
-                        Type = ResourceType.IIIFManifest,
-                        Canonical = true
-                    }
-                ]
-            });
-        }
+        // Child manifest
+        await DbContext.Manifests.AddAsync(new Manifest
+        {
+            Id = "FirstChildManifest",
+            CustomerId = CustomerId,
+            Created = DateTime.UtcNow,
+            Modified = DateTime.UtcNow,
+            CreatedBy = "admin",
+            Hierarchy =
+            [
+                new Hierarchy
+                {
+                    Slug = "iiif-manifest",
+                    Parent = RootCollection.Id,
+                    Type = ResourceType.IIIFManifest,
+                    Canonical = true
+                }
+            ],
+            LastProcessed = DateTime.UtcNow
+        });
+        
+        // processing child manifest
+        await DbContext.Manifests.AddAsync(new Manifest
+        {
+            Id = "FirstChildManifestProcessing",
+            CustomerId = CustomerId,
+            Created = DateTime.UtcNow,
+            Modified = DateTime.UtcNow,
+            CreatedBy = "admin",
+            Hierarchy =
+            [
+                new Hierarchy
+                {
+                    Slug = "iiif-manifest-processing",
+                    Parent = RootCollection.Id,
+                    Type = ResourceType.IIIFManifest,
+                    Canonical = true
+                }
+            ]
+        });
         
         await DbContext.SaveChangesAsync();
     }
@@ -311,8 +329,8 @@ public class PresentationContextFixture : IAsyncLifetime
     public void CleanUp()
     {
         DbContext.Database.ExecuteSqlRaw(
-            "DELETE FROM collections WHERE customer_id NOT IN (1, 10) AND id NOT IN ('root','FirstChildCollection','SecondChildCollection', 'NonPublic', 'IiifCollection')");
+            "DELETE FROM collections WHERE customer_id != 1 AND id NOT IN ('root','FirstChildCollection','SecondChildCollection', 'NonPublic', 'IiifCollection')");
         DbContext.Database.ExecuteSqlRaw(
-            "DELETE FROM manifests WHERE customer_id NOT IN (1, 10) AND id NOT IN ('FirstChildManifest', 'FirstChildManifestProcessing')");
+            "DELETE FROM manifests WHERE customer_id != 1 AND id NOT IN ('FirstChildManifest', 'FirstChildManifestProcessing')");
     }
 }
