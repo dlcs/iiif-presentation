b5da45610deff77214cffbc25d96a0278cfc8763
jack.lewis <jack.lewis@digirati.com>
2025-06-24 12:01:10 +0100

    add tests to JObjectX

--- src/IIIFPresentation/Core.Tests/Helpers/JObjectXTests.cs
+++ src/IIIFPresentation/Core.Tests/Helpers/JObjectXTests.cs
@@ -21,4 +21,20 @@ public class JObjectXTests
         Action action = () => jobject.GetRequiredValue("foo");
         action.Should().ThrowExactly<InvalidOperationException>().WithMessage("Object missing 'foo' property");
     }
+    
+    [Fact]
+    public void TryGetRequiredValue_ReturnsValue_IfFound()
+    {
+        var jobject = JObject.Parse("{ \"name\": \"John Doe\" }");
+
+        jobject.TryGetValue("name").ToString().Should().BeEquivalentTo("John Doe");
+    }
+    
+    [Fact]
+    public void TryGetRequiredValue_Throws_IfNotFound()
+    {
+        var jobject = JObject.Parse("{ \"name\": \"John Doe\" }");
+
+        jobject.TryGetValue("foo").Should().BeNull();
+    }
 }
