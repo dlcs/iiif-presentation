a5de12a102dedd88d452e9088e0f23935cc92555
p-kaczynski <p.a.kaczynski@gmail.com>
2025-06-25 18:18:38 +0100

    Add `ETagComparer` to compare stored `Guid` with a string value from request's headers

---
+++ src/IIIFPresentation/API/Infrastructure/Helpers/EtagComparer.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Infrastructure.Helpers;
+
+public class EtagComparer
+{
+    public static bool IsMatch(Guid storedEtag, string? incomingEtag)
+    => incomingEtag is {Length:>0} && Guid.TryParse(incomingEtag.Trim('"'), out var guid) && storedEtag == guid;
+}
