158921b7b6b83d5e74f82f9105d8ddb686d74951
jack.lewis <jack.lewis@digirati.com>
2025-08-12 12:21:32 +0100

    remove no longer valid test

--- src/IIIFPresentation/Services.Tests/Manifests/ManifestItemsParserTests.cs
+++ src/IIIFPresentation/Services.Tests/Manifests/ManifestItemsParserTests.cs
@@ -46,55 +46,6 @@ public class ManifestItemsParserTests
                 Items = [new Canvas { Items = [new AnnotationPage { Items = [new TypeClassifyingAnnotation()] }] }]
             }, 123)
             .Should().HaveCount(1);
-
-    [Fact]
-    public async Task Parse_Throws_CanvasIdInvalidUri()
-    {
-        // Arrange
-        var manifest = @"
-{
-    ""@context"": ""http://iiif.io/api/presentation/3/context.json"",
-    ""id"": ""https://iiif.io/api/cookbook/recipe/0001-mvm-image/manifest.json"",
-    ""type"": ""Manifest"",
-    ""items"": [
-        {
-            ""id"": ""i-am-not-a-uri"",
-            ""type"": ""Canvas"",
-            ""height"": 1800,
-            ""width"": 1200,
-            ""items"": [
-                {
-                    ""id"": ""https://iiif.io/api/cookbook/recipe/0001-mvm-image/page/p1/1"",
-                    ""type"": ""AnnotationPage"",
-                    ""items"": [
-                        {
-                            ""id"": ""https://iiif.io/api/cookbook/recipe/0001-mvm-image/annotation/p0001-image"",
-                            ""type"": ""Annotation"",
-                            ""motivation"": ""painting"",
-                            ""body"": {
-                                ""id"": ""http://iiif.io/api/presentation/2.1/example/fixtures/resources/page1-full.png"",
-                                ""type"": ""Image"",
-                                ""format"": ""image/png"",
-                                ""height"": 1800,
-                                ""width"": 1200
-                            },
-                            ""target"": ""https://iiif.io/api/cookbook/recipe/0001-mvm-image/canvas/p1""
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-}";
-        
-        var deserialised = await manifest.ToPresentation<PresentationManifest>();
-         
-        // Act
-        Action action = () => sut.ParseToCanvasPainting(deserialised, 123);
-        
-        // Assert
-        action.Should().Throw<UriFormatException>();
-    }
     
     [Fact]
     public async Task Parse_Throws_MissingBody()
