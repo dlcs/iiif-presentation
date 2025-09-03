b6aad10c7b64a4e1d4b069fc482d7f3551c71ffc
jack.lewis <jack.lewis@digirati.com>
2025-09-03 14:36:45 +0100

    move to use nullable bool

--- src/IIIFPresentation/Models/API/Manifest/PresentationManifest.cs
+++ src/IIIFPresentation/Models/API/Manifest/PresentationManifest.cs
@@ -50,13 +50,7 @@ public class PaintedResource
     [JsonProperty(Order = 3)]
     public JObject? Asset { get; set; }
     
-    public bool Reingest { get; set; }
-    
-    // Newtonsoft method to stop serialization, but allow deserialization of the reingest property
-    public bool ShouldSerializeReingest()
-    {
-        return false;
-    }
+    public bool? Reingest { get; set; }
 }
 
 public class CanvasPainting
