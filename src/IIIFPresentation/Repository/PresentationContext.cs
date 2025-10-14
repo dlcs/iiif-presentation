d48ff6b1b029a64454d79198d4ef5b6a7da4bee1
jack.lewis <jack.lewis@digirati.com>
2025-10-14 15:47:15 +0100

    Add additional index to CanvasPainting

--- src/IIIFPresentation/Repository/PresentationContext.cs
+++ src/IIIFPresentation/Repository/PresentationContext.cs
@@ -120,6 +120,9 @@ public class PresentationContext : DbContext
                     { cp.Id, cp.CustomerId, cp.ManifestId, cp.AssetId, cp.CanvasOrder, cp.ChoiceOrder })
                 .IsUnique()
                 .HasFilter("canvas_original_id is null");
+            entity.HasIndex(cp => new
+                    { cp.Id, cp.CustomerId, cp.ManifestId, cp.AssetId, cp.CanvasOriginalId, cp.CanvasOrder, cp.ChoiceOrder })
+                .IsUnique();
 
             entity.Property(cp => cp.AssetId)
                 .HasConversion(id => id!.ToString(), id => AssetId.FromString(id));
