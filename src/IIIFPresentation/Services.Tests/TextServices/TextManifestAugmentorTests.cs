45ca4904dd202e6aaf7002897f0f1a44361ed658
jack.lewis <jack.lewis@digirati.com>
2026-09-15 12:48:33 +0100

    Allow text augmentor to support rendering and annotations being output on the manifest

--- src/IIIFPresentation/Services.Tests/TextServices/TextManifestAugmentorTests.cs
+++ src/IIIFPresentation/Services.Tests/TextServices/TextManifestAugmentorTests.cs
@@ -1,7 +1,8 @@
 using FakeItEasy;
-using IIIF;
 using IIIF.Presentation;
 using IIIF.Presentation.V3;
+using IIIF.Presentation.V3.Annotation;
+using IIIF.Presentation.V3.Content;
 using IIIF.Search.V2;
 using Microsoft.Extensions.Logging.Abstractions;
 using DbManifest = Models.Database.Collections.Manifest;
@@ -49,6 +50,125 @@ public class TextManifestAugmentorTests
 
         result.Should().BeSameAs(manifest);
         result.Service.Should().BeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Augment_AddsRendering_ToManifest()
+    {
+        var pdfRendering = new ExternalResource("Text") { Id = "https://example.com/pdf/v1" };
+        var augmented = new Manifest { Rendering = [pdfRendering] };
+        A.CallTo(() => textSearchClient.GetTextAugmentedManifest(A<TextJobId>._, A<CancellationToken>._))
+            .Returns(Task.FromResult<Manifest?>(augmented));
+
+        var manifest = new Manifest { Id = "https://example.com/manifest" };
+
+        var result = await sut.Augment(manifest, DbManifest, CancellationToken.None);
+
+        result.Rendering.Should().ContainSingle(r => r.Id == "https://example.com/pdf/v1");
+    }
+
+    [Fact]
+    public async Task Augment_DoesNotDuplicateRendering_WhenAlreadyPresentOnManifest()
+    {
+        var pdfRendering = new ExternalResource("Text") { Id = "https://example.com/pdf/v1" };
+        var augmented = new Manifest { Rendering = [pdfRendering] };
+        A.CallTo(() => textSearchClient.GetTextAugmentedManifest(A<TextJobId>._, A<CancellationToken>._))
+            .Returns(Task.FromResult<Manifest?>(augmented));
+
+        var manifest = new Manifest
+        {
+            Id = "https://example.com/manifest",
+            Rendering = [new ExternalResource("Text") { Id = "https://example.com/pdf/v1" }]
+        };
+
+        var result = await sut.Augment(manifest, DbManifest, CancellationToken.None);
+
+        result.Rendering.Should().ContainSingle();
+    }
+
+    [Fact]
+    public async Task Augment_AddsAnnotations_ToManifest()
+    {
+        var annotationPage = new AnnotationPage { Id = "https://example.com/annotations/1/v1" };
+        var augmented = new Manifest { Annotations = [annotationPage] };
+        A.CallTo(() => textSearchClient.GetTextAugmentedManifest(A<TextJobId>._, A<CancellationToken>._))
+            .Returns(Task.FromResult<Manifest?>(augmented));
+
+        var manifest = new Manifest { Id = "https://example.com/manifest" };
+
+        var result = await sut.Augment(manifest, DbManifest, CancellationToken.None);
+
+        result.Annotations.Should().ContainSingle(a => a.Id == "https://example.com/annotations/1/v1");
+    }
+
+    [Fact]
+    public async Task Augment_AddsCanvasAnnotations_ToMatchingCanvas()
+    {
+        var linesRef = new AnnotationPage { Id = "https://example.com/annotations/lines/0/v1" };
+        var wordsRef = new AnnotationPage { Id = "https://example.com/annotations/words/0/v1" };
+        var augmented = new Manifest
+        {
+            Items = [new Canvas { Id = "https://example.com/canvas/1", Annotations = [linesRef, wordsRef] }]
+        };
+        A.CallTo(() => textSearchClient.GetTextAugmentedManifest(A<TextJobId>._, A<CancellationToken>._))
+            .Returns(Task.FromResult<Manifest?>(augmented));
+
+        var manifest = new Manifest
+        {
+            Id = "https://example.com/manifest",
+            Items = [new Canvas { Id = "https://example.com/canvas/1" }]
+        };
+
+        var result = await sut.Augment(manifest, DbManifest, CancellationToken.None);
+
+        result.Items!.Single().Annotations.Should().BeEquivalentTo([linesRef, wordsRef]);
+    }
+
+    [Fact]
+    public async Task Augment_DoesNotAddCanvasAnnotations_WhenNoCanvasIdMatches()
+    {
+        var linesRef = new AnnotationPage { Id = "https://example.com/annotations/lines/0/v1" };
+        var augmented = new Manifest
+        {
+            Items = [new Canvas { Id = "https://example.com/canvas/other", Annotations = [linesRef] }]
+        };
+        A.CallTo(() => textSearchClient.GetTextAugmentedManifest(A<TextJobId>._, A<CancellationToken>._))
+            .Returns(Task.FromResult<Manifest?>(augmented));
+
+        var manifest = new Manifest
+        {
+            Id = "https://example.com/manifest",
+            Items = [new Canvas { Id = "https://example.com/canvas/1" }]
+        };
+
+        var result = await sut.Augment(manifest, DbManifest, CancellationToken.None);
+
+        result.Items!.Single().Annotations.Should().BeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Augment_AddsSearchServiceRenderingAndAnnotations_WhenAllPresent()
+    {
+        var searchService = new SearchService2 { Id = "https://example.com/search" };
+        var pdfRendering = new ExternalResource("Text") { Id = "https://example.com/pdf/v1" };
+        var annotationPage = new AnnotationPage { Id = "https://example.com/annotations/1/v1" };
+        var augmented = new Manifest
+        {
+            Service = [searchService],
+            Rendering = [pdfRendering],
+            Annotations = [annotationPage]
+        };
+        A.CallTo(() => textSearchClient.GetTextAugmentedManifest(A<TextJobId>._, A<CancellationToken>._))
+            .Returns(Task.FromResult<Manifest?>(augmented));
+
+        var manifest = new Manifest { Id = "https://example.com/manifest" };
+        manifest.EnsurePresentation3Context();
+
+        var result = await sut.Augment(manifest, DbManifest, CancellationToken.None);
+
+        result.Service.Should().ContainSingle(s => s.Id == "https://example.com/search");
+        result.Rendering.Should().ContainSingle(r => r.Id == "https://example.com/pdf/v1");
+        result.Annotations.Should().ContainSingle(a => a.Id == "https://example.com/annotations/1/v1");
     }
 
     [Fact]
