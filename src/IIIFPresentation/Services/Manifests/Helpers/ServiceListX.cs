66f4e8ba93b3a19f92ac46bd21d741c9dcb471ea
jack.lewis <jack.lewis@digirati.com>
2026-09-18 12:45:06 +0100

    Revert "Apply services block for authenticated assets"
    This reverts commit be5bcaecc2e9093af9c1612ea7e550bac50d4535.

--- src/IIIFPresentation/Services/Manifests/Helpers/ServiceListX.cs
+++ src/IIIFPresentation/Services/Manifests/Helpers/ServiceListX.cs
@@ -1,9 +1,7 @@
 ﻿using Core.Helpers;
 using IIIF;
-using IIIF.Auth.V2;
 using IIIF.ImageApi.V2;
 using IIIF.ImageApi.V3;
-using IIIF.Presentation.V3;
 
 namespace Services.Manifests.Helpers;
 
@@ -21,39 +19,5 @@ public static class ServiceListX
             return (is2.Width, is2.Height);
 
         return null;
-    }
-
-    /// <summary>
-    /// Recursively walks <paramref name="services"/> (and any services nested within them, e.g. an
-    /// ImageService3.Service containing an AuthProbeService2) collecting the ids referenced by any
-    /// <see cref="AuthProbeService2"/> found. These reference the 'full' AuthAccessService2 definitions held at
-    /// Manifest level.
-    /// </summary>
-    public static HashSet<string> GetReferencedAuthServiceIds(this IEnumerable<IService>? services)
-    {
-        var referencedIds = new HashSet<string>();
-        CollectReferencedAuthServiceIds(services, referencedIds);
-        return referencedIds;
-    }
-
-    private static void CollectReferencedAuthServiceIds(IEnumerable<IService>? services, HashSet<string> referencedIds)
-    {
-        if (services == null) return;
-
-        foreach (var service in services)
-        {
-            if (service is AuthProbeService2 probeService)
-            {
-                foreach (var reference in probeService.Service ?? [])
-                {
-                    if (reference.Id != null) referencedIds.Add(reference.Id);
-                }
-            }
-
-            if (service is ResourceBase { Service: { } nestedServices })
-            {
-                CollectReferencedAuthServiceIds(nestedServices, referencedIds);
-            }
-        }
     }
 }
